b8f3aca1e2d8917c93db16a3efe36768ee379601
Braun Roland <rolibraun94@gmail.com>
2025-02-14 11:30:40 +0100

    3. feladat kész

--- fajlbeolvasas/Program.cs
+++ fajlbeolvasas/Program.cs
@@ -6,10 +6,11 @@
         {
             List<Karakter> karakterek = [];
             Beolvasas("karakterek.txt", karakterek);
-            foreach (var item in karakterek)
-            {
-                Console.WriteLine(item.ToString());
-            }
+            //foreach (var item in karakterek)
+            //{
+            //    Console.WriteLine(item.ToString());
+            //}
+            F03(karakterek);
         }
 
         static void Beolvasas(string fajlnev, List<Karakter> karakterek)
@@ -23,6 +24,17 @@
                 Karakter karakter = new(szavak[0], Convert.ToInt16(szavak[1]), Convert.ToInt16(szavak[2]), Convert.ToInt16(szavak[3]));
                 karakterek.Add(karakter);
             }
+        }
+
+        static void F03(List<Karakter> karakterek)
+        {
+            int atlag = 0;
+            foreach (var item in karakterek)
+            {
+                atlag += item.Szint;
+            };
+            Console.WriteLine("3. feladat: szint átlaga");
+            Console.WriteLine($"- {atlag/karakterek.Count}");
         }
     }
 }
